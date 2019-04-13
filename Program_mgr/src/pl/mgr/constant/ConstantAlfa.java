93de8f5605e70fa5203fd9fd7e317f403e749f20
vainamonen <vainamonen@gmail.com>
2019-04-13 11:00:06 +0200

    format code

--- Program_mgr/src/pl/mgr/constant/ConstantAlfa.java
+++ Program_mgr/src/pl/mgr/constant/ConstantAlfa.java
@@ -4,20 +4,20 @@ public class ConstantAlfa {
     public static final int E = 999;
 
 
-    public static double dE,dx;
+    public static double dE, dx;
 
 
-    public static double K=307000;//stala w eV
-    public static double mel=511000;//masa(energia) elektronu[eV]
-    public static double c=3*Math.pow(10,8); // prędkość światła
-    public static double Z=82;//liczba atomowa absorbentu np. olow=82
-    public static double z=2; //liczba atomowa czastki
-    public static double A=207.2;//masa atomowa czastki
+    public static double K = 307000;//stala w eV
+    public static double mel = 511000;//masa(energia) elektronu[eV]
+    public static double c = 3 * Math.pow(10, 8); // prędkość światła
+    public static double Z = 82;//liczba atomowa absorbentu np. olow=82
+    public static double z = 2; //liczba atomowa czastki
+    public static double A = 207.2;//masa atomowa czastki
     public static double ro = 11.34;//gęstość np. ołowiu=11.34[g/cm^3]
-    public static double D= 44; //grubość absorbentu z paska [g/cm^2](objętość)
-    public static double Dcm = D/ro; // grubość absorbentu w [cm]
+    public static double D = 44; //grubość absorbentu z paska [g/cm^2](objętość)
+    public static double Dcm = D / ro; // grubość absorbentu w [cm]
     public static double V = 15000000; //prędkość cząstki alfa [m/s]
-    public static double M=3700; //masa cząstki [g]
+    public static double M = 3700; //masa cząstki [g]
 
     public static double Ek = 100;
 
